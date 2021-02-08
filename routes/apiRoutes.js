a5d2c6a2466d0df654ad5777a70e6cc141d434f2
pfotis <69961508+pfotis@users.noreply.github.com>
2021-02-08 16:43:58 +1100

    write functions app.get and app.post

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,5 @@
 // LOAD DATA
-
+const fs = require("fs");
 const db = require('../db/db');
 
 // ROUTING
@@ -12,8 +12,37 @@ module.exports = (app) => {
 
   // ---------------------------------------------------------------------------
   
-  app.post('/api/notes', (req, res) => {
-    db.push(req.body);
-    res.json(true);
+  app.get("/api/notes", function(err, res) {
+    try {
+      notesData = fs.readFileSync("./db/db.json", "utf8");
+      notesData = JSON.parse(notesData);
+    } catch (err) {
+      console.log("\n error (in app.get.catch):");
+      console.log(error);
+    }
+    res.json(notesData);
+  });
+
+  app.post("/api/notes", function(req, res) {
+    try {
+      notesData = fs.readFileSync("./db/db.json", "utf8");
+      console.log(notesData);
+      notesData = JSON.parse(notesData);
+      req.body.id = notesData.length;
+      notesData.push(req.body); 
+      notesData = JSON.stringify(notesData);
+    
+      fs.writeFile("./db/db.json", notesData, "utf8", function(err) {
+        if (err) 
+          throw err;
+      });
+
+      res.json(JSON.parse(notesData));
+  
+      // error Handling
+    } catch (err) {
+      throw err;
+      console.log(error);
+    }
   });
 }
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,5 +1,4 @@
 // DEPENDENCIES
-// We need to include the path package to get the correct file path for our html
 
 const path = require('path');
 
