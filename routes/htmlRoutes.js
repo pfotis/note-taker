91fe36217a00ce97ec7cd1b6bf1c862f651ea038
pfotis <69961508+pfotis@users.noreply.github.com>
2021-02-08 16:08:40 +1100

    create file db.json

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -18,4 +18,8 @@ module.exports = (app) => {
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
   });
+
+  app.get("/api/notes", function(req, res) {
+    return res.sendFile(path.json(__dirname, "Develop/db/db.json"));
+  });
 };
